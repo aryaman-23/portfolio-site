c461937a83a965aaf0e90425cca238d56a3df36f
aryaman <aryaman@railofy.com>
2024-12-14 00:32:26 +0530

    Added astro inital commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,20 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Aryaman Agarwal',
+			description: "", 
+			tagline: "Life of Aryaman", 
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/aryaman-23',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			topNav: 
+				{ text: 'About', link: '/' },
+				// { text: 'Blog', link: '/blog' },
+			customCss: ['./src/styles/global.css']
 		}),
+
 	],
 });
